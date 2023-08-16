73c0d0bee6540bf4a065f4b092629f6bf2b0ae4d
Camila <97048604+camibarbosa@users.noreply.github.com>
2023-08-15 21:35:19 -0300

    adicionando a propriedade "key"

--- src/app/pages/Contato/page.jsx
+++ src/app/pages/Contato/page.jsx
@@ -84,17 +84,18 @@ export default function Contato() {
       </form>
 
       <div className="bg-slate-100 flex flex-col">
-        {error &&
-          error.map((e) => (
-            <div
-              className={`${
-                success ? "text-green-800" : "text-red-600"
-              } px-5 py-2`}
-            >
-              {e}
-            </div>
-          ))}
+  {error &&
+    error.map((e, index) => (
+      <div
+        key={index}
+        className={`${
+          success ? "text-green-800" : "text-red-600"
+        } px-5 py-2`}
+      >
+        {e}
       </div>
+    ))}
+</div>
     </>
   );
 }
